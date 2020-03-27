e3da15836237a0610116166afcb5b01e072bb3d6
eikendev <raphael@eiken.dev>
2020-03-27 23:44:15 +0100

    Remove PurgeCSS
    This plugin caused removed necessary styles that were dynamically
    applied with JavaScript. There are ways to whitelist certain rules, but
    I don't think there is a generalized enough way for this problem. Since
    we serve Bootstrap on our own now, this should not be a severe problem.

--- assets/scss/postcss.config.js
+++ assets/scss/postcss.config.js
@@ -1,13 +1,5 @@
 module.exports = {
 	plugins: [
 		require('autoprefixer'),
-		...process.env.HUGO_ENVIRONMENT === 'production'
-			? [
-				require('@fullhuman/postcss-purgecss')({
-					content: [
-						"./layouts/**/*.html",
-					],
-				})
-			] : []
 	]
 }
