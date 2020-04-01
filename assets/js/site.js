50ae516ab2238a84ac715f6c419c8787722e5e00
eikendev <raphael@eiken.dev>
2020-04-01 14:51:50 +0200

    Add shortcode to create charts with Chart.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,3 +3,17 @@ MathJax = {
 		fontURL: '/font/mathjax',
 	},
 };
+
+if (typeof ChartjsLoaders === 'undefined') {
+	var ChartjsLoaders = new Array();
+}
+
+$(document).ready(function() {
+	ChartjsLoaders.forEach(function (item, index) {
+		var scriptTag = document.createElement('script');
+		scriptTag.setAttribute('data-name', item);
+		firstScriptTag = document.getElementsByTagName('script')[0];
+		scriptTag.src = item + ".js";
+		firstScriptTag.parentNode.insertBefore(scriptTag, firstScriptTag);
+	});
+});
